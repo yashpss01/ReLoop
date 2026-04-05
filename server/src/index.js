47d3155f122ce0bfa923ea4d8a1a620ece474ab9
Yash Pratap Singh Solanki <yashpratap0143@gmail.com>
2026-04-05 22:52:50 +0530

    bug: database connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 const app = require('./app');
 
 const PORT = process.env.PORT || 5001;
